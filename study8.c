ef81e7b72f77571b2cfbd283e96b9585a3309d9b
Gülden Öykü Demirkol <68506808+oyku-demirkol@users.noreply.github.com>
2021-07-11 13:22:45 +0300

    Update study8.c

--- study8.c
+++ study8.c
@@ -18,7 +18,7 @@ int main() {
   int day = 0;
   printf("please enter the day value as a number in the range of [1,7] :");
   scanf("%d", &day);
-  if (day < 1 || day > 7) day = 0; // (|| = or )
+  if (day < 1 || day > 7) day = 0; // (|| = The logical or )
   printf("%s\n", days[day]); // Here we print the day as a string according to the index of the day entered from the days array.
 
   return 0;
