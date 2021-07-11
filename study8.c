72ddb2619046f1754cd76f12e136445fe9c143b8
Gülden Öykü Demirkol <68506808+oyku-demirkol@users.noreply.github.com>
2021-07-11 13:15:33 +0300

    Add files via upload

---
+++ study8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Days would be written as an array
+
+int main() {
+  char *days[] = {
+    "Invalid",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  };
+
+  int day = 0;
+  printf("please enter the day value as a number in the range of [1,7] :");
+  scanf("%d", &day);
+  if (day < 1 || day > 7) day = 0; // (|| = or )
+  printf("%s\n", days[day]); // Here we print the day as a string according to the index of the day entered from the days array.
+
+  return 0;
+}
