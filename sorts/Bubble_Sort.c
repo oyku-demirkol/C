9ba1fdbac943e2f93ec6d0e1d96cb801a7726609
Gülden Öykü Demirkol <68506808+oyku-demirkol@users.noreply.github.com>
2021-08-13 21:35:16 +0300

    Update Bubble_Sort.c

--- sorts/Bubble_Sort.c
+++ sorts/Bubble_Sort.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #define MAX 50
 
+/* Bubble sort is a algorithm for arranging a string of numbers or other elements in the correct order. 
+The method works by examining each set of adjacent elements in the string, from left to right, switching their positions if they are out of order. 
+The algorithm then repeats this process until it can run through the entire string and find no two elements that need to be swapped.*/
+
 void bubblesort(int arr[] , int size ){
     int i,j;
     for ( i =0; i<size; i++ ){
