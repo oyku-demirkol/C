5945e0c449f4e3dec45a2b2e100ae0c84fe0defa
Gülden Öykü Demirkol <68506808+oyku-demirkol@users.noreply.github.com>
2021-11-07 15:51:08 +0300

    Add files via upload

---
+++ InitializingArray.c
@@ -0,0 +1,22 @@
+//Initializing the elements of an array to zeros
+#include <stdio.h>
+#include <stdlib.h>
+
+//function main begins program execution
+//size_t is a type guaranteed to hold any array index.
+int main()
+{
+    int n[5]; // n is an array of five integers
+
+    //set elements of array n to 0
+    for(size_t i = 0; i<5; ++i){
+        n[i]=0; // set element at location i to 0
+    }
+    printf("%s%13s\n", "Element","Value");
+    //output contents of array n in tabular format
+    for(size_t i = 0; i<5; i++){
+        printf("%7u%13d\n",i,n[i]);
+    }
+
+    return 0;
+}
