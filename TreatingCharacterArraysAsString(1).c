e38e2fb6d96bfa5919165bd73b88e8d55faf9790
Gülden Öykü Demirkol <68506808+oyku-demirkol@users.noreply.github.com>
2021-11-09 23:32:38 +0300

    Add files via upload

---
+++ TreatingCharacterArraysAsString(1).c
@@ -0,0 +1,26 @@
+//Treating character arrays as string
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 20
+
+// function main begins program execution
+int main()
+{
+    char string1[SIZE]; //reserves 20 characters
+    char string2[] = "string literal";
+
+    //read string from user into array string1
+    printf("%s", "Enter a string (no longer than 19 characters): ");
+    scanf("%19s", string1);
+
+    //output strings
+    printf("string1 is: %s\nstring2 is: %s\n"
+           "string1 with spaces between charachter is:\n",string1,string2);
+     //output characters until null character is reached
+     for(size_t i=0; i<SIZE && string1[i] != '\0'; ++i){  //space = '\0'
+        printf("%c ", string1[i]);
+     }
+     puts("");
+
+    return 0;
+}
