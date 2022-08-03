e6bfbf3c1a35b9ea769bc41abe1d8f35037c817f
Gülden Öykü Demirkol <68506808+oyku-demirkol@users.noreply.github.com>
2022-08-03 14:13:15 +0200

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int main()
+{
+   
+    std::cout << sizeof(double) << "\n";        //8
+    std::cout << sizeof(long)<< "\n";           //4
+    std::cout << sizeof(short) <<"\n";          //2
+    std::cout << sizeof(int) << "\n";           //4
+    std::cout << sizeof(char) << "\n";          //1
+    std::cout << sizeof(const long) << "\n";    //4
+    std::cout << sizeof(unsigned char) << "\n"; //1
+    std::cout << sizeof(float) << "\n";         //4
+
+}
+
